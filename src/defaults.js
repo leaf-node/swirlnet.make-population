a9ba35616f758878013d042f035f0e4436df1b83
Andrew Engelbrecht <ae@n0r.co>
2016-08-28 15:37:06 -0400

    updated default settings comments

--- src/defaults.js
+++ src/defaults.js
@@ -14,10 +14,10 @@
 
 var defaults;
 
-// These are the default settings for the genetic algorithm. If you merely wish
-// to use different values, use the settings argument of makePopulation(). If
-// you belive that the defaults should be changed for everyone, consider making
-// a patch and pushing your changes upstream.
+// These are the default settings for the genetic algorithm. If you wish to use
+// different values, use the settings argument of makePopulation(). If you
+// belive that the defaults should be changed for everyone by default then feel
+// free to let me know.
 
 defaults = {};
 
@@ -32,13 +32,14 @@ defaults.survivalThreshold                  = 0.2;
 // when species population is greater than this, the speicies champion is copied unmodified
 defaults.championCloneThreshold             = 5;
 
-// constants used in speciation equation
+// constants used to determine how different two genomes are
 defaults.disjointCoefficient                = 1.0;
 defaults.excessCoefficient                  = 1.0;
 defaults.weightDifferenceCoefficient        = 0.4;
 
-// also used in speciation equation
+// threshold over which two genomes are considered different species
 defaults.compatibilityThreshold             = 3.0;
+// the amount by which this threshold changes per generation
 defaults.compatibilityModifier              = 0.0;
 
 // rate at which genomes are selected to mutate any of their connection weights
@@ -58,15 +59,19 @@ defaults.addLinkMutationRate                = 0.05;
 // rate at which genomes reproduce asexually only
 defaults.asexualReproductionOnlyRate        = 0.25;
 
-// rate at which matching genes are selected from fitter genome
+// rate at which matching genes are inherited from the fitter genome
 defaults.fitterGenomeInheritanceRate        = 0.5;
 
+
+// To understand the impact of gene disablement options, read the VS-NEAT.md
+// file in this repository.
 
 // rate of random connection gene disablement toggling
 defaults.randomDisablementRate              = 0.00;
 defaults.randomEnablementRate               = 0.00;
 
 // rate at which gene enablement in a fitter genome is forced onto the child's gene
+// (regardless of which parent the rest of the gene with its connection weight was inherited from)
 defaults.fitterGenomeEnablementOverrideRate     = 1.0;
 // rate at which gene disablement in a weaker genome is forced onto the child's gene
 defaults.weakerGenomeDisablementOverrideRate    = 0.0;
@@ -81,7 +86,7 @@ defaults.neitherGenomeEnablementOverrideRate    = 0.0;
 
 
 // settings for network functionality
-//
+
 // steepness of sigmoidal curve used by network nodes to modulate input
 defaults.sigmoidSteepness                   = 4.9;
 // the automatic "bias" node input value (should not be 0)
